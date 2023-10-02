d44d82a7f0975208e2309e97222ad294165d7e61
ArthurHenriqueSchlikmann <arthursh@outlook.com.br>
2023-10-02 18:22:34 -0300

    v16

--- src/com/sitesdoarthur/program/interface_interna.java
+++ src/com/sitesdoarthur/program/interface_interna.java
@@ -5,10 +5,11 @@ import java.util.Scanner;
 public class interface_interna {
     public static void main(String[] args) {
         System.out.print("root> ");
-        Scanner teclado = new Scanner(System.in);
-        String commando = teclado.nextLine();
-        if("boot".equals(commando)) {
-            System.out.println("Em desenvlvimento");
+        try (Scanner teclado = new Scanner(System.in)) {
+            String commando = teclado.nextLine();
+            if("boot".equals(commando)) {
+                System.out.println("Em desenvlvimento");
+            }
         }
     }
 }
