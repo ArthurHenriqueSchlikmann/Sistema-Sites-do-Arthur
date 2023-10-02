5e2da54e2b5495ee2aed34b6170cc4f0d43db5bc
ArthurHenriqueSchlikmann <arthursh@outlook.com.br>
2023-10-02 17:25:25 -0300

    v5

--- src/com/sitesdoarthur/java/sistema.java
+++ src/com/sitesdoarthur/java/sistema.java
@@ -1,6 +1,10 @@
 package com.sitesdoarthur.java;
 
 public class sistema {
+    /**
+     * @author ArthurHenriqueSchlikmann
+     * @version 1b
+     */
     private void main() {
         System.out.println("INICIANDO...");
     }
