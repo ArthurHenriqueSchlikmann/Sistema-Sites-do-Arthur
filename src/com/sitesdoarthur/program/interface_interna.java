d5e75056bc6e7ac3f8578d7a2bbefc0e7d8f7933
ArthurHenriqueSchlikmann <arthursh@outlook.com.br>
2023-10-02 18:00:18 -0300

    v13

--- src/com/sitesdoarthur/program/interface_interna.java
+++ src/com/sitesdoarthur/program/interface_interna.java
@@ -4,6 +4,9 @@ import java.util.Scanner;
 
 public class interface_interna {
     public static void main(String[] args) {
+        System.out.print("root> ");
         Scanner teclado = new Scanner(System.in);
+        String commando = teclado.nextLine();
+        if("boot".equals(commando)) {}
     }
 }
