b9a061d2e173a21af5d5590d5ad54bd26810023e
ArthurHenriqueSchlikmann <arthursh@outlook.com.br>
2023-10-09 19:35:48 -0300

    v40

--- src/com/sitesdoarthur/calculadora/calculadora.java
+++ src/com/sitesdoarthur/calculadora/calculadora.java
@@ -2,6 +2,6 @@ package com.sitesdoarthur.calculadora;
 
 public class calculadora {
     public static void main(String[] args) {
-        
+        System.out.println("-----------------------------------");
     }
 }
