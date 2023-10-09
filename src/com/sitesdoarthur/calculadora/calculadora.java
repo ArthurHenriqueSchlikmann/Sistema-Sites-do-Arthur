c9df425aa940ffdafe02ca01008185fa6d1e6409
ArthurHenriqueSchlikmann <arthursh@outlook.com.br>
2023-10-09 20:23:15 -0300

    v41

--- src/com/sitesdoarthur/calculadora/calculadora.java
+++ src/com/sitesdoarthur/calculadora/calculadora.java
@@ -1,7 +1,21 @@
 package com.sitesdoarthur.calculadora;
 
+import java.util.Scanner;
+
 public class calculadora {
     public static void main(String[] args) {
-        System.out.println("-----------------------------------");
+        System.out.println("------------------------------------");
+        System.out.println("-------CALCULADORA INTERATIVA-------");
+        System.out.println("------------------------------------");
+        int num;
+        byte menu;
+        try (Scanner teclado = new Scanner(System.in)) {
+            System.out.println("Digite [1] para ADIÇÃO");
+            System.out.println("Digite [2] para SUBTRAÇÃO");
+            System.out.println("Digite [3] para MULTIPLICAÇÃO");
+            System.out.println("Digite [4] para DIVISÃO");
+            System.out.println("E precione [ENTER] para confirmar");
+            menu = teclado.nextByte();
+        }
     }
 }
