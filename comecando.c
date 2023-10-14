7d96e6358efae622d96a116b74c17ba2749884d7
ArthurHenriqueSchlikmann <arthursh@outlook.com.br>
2023-10-14 12:20:10 -0300

    v55

--- comecando.c
+++ comecando.c
@@ -4,4 +4,5 @@
 int main() {
     printf("para começar abra systema.jar e digite um usuário qualquer ");
     printf("depois digite help na pasta root> \n");
+    system("pause");
 }
--- src/com/sitesdoarthur/aditionals/dados.java
+++ src/com/sitesdoarthur/aditionals/dados.java
@@ -1,9 +1,10 @@
 package com.sitesdoarthur.aditionals;
 public class dados {
     /**
-     * 
+     * @author ArthurHenriqueSchlikmann
+     * @version 1.0
      */
     public static void main(String[] args) {
-        
+
     }
 }
