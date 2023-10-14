812e6278bd704734d99d2c49a382a557cd14bb3d
ArthurHenriqueSchlikmann <arthursh@outlook.com.br>
2023-10-14 11:23:15 -0300

    v49

---
+++ comecando.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    printf("para começar abra systema.jar e digite um usuário qualquer ");
+    printf("depois digite help na pasta root> \n");
+}
