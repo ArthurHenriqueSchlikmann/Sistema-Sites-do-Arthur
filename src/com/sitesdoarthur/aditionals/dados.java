e25eb5ce6cf7ef3eab63535d2c3230d4ddef6c67
ArthurHenriqueSchlikmann <arthursh@outlook.com.br>
2023-10-14 12:18:40 -0300

    v54

--- src/com/sitesdoarthur/aditionals/dados.java
+++ src/com/sitesdoarthur/aditionals/dados.java
@@ -1,9 +1,9 @@
 package com.sitesdoarthur.aditionals;
-import org.json
-
 public class dados {
-
+    /**
+     * 
+     */
     public static void main(String[] args) {
-        JsonReader reader = ;
+        
     }
 }
