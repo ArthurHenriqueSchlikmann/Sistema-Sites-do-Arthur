1e37999fe39a5262bc42e6746d099f7d88a3dcf0
ArthurHenriqueSchlikmann <arthursh@outlook.com.br>
2023-10-17 17:29:18 -0300

    v51

--- src/com/sitesdoarthur/aditionals/dados.java
+++ src/com/sitesdoarthur/aditionals/dados.java
@@ -1,9 +1,13 @@
 package com.sitesdoarthur.aditionals;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class dados {
     /**
      * @author ArthurHenriqueSchlikmann
      * @version 1.0
      */
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        
+    }
 }
